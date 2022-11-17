7cc9b038fbc9b67daff95976a3149c8da95c28ec
Noah Riley Lisk <noahrileylisk@Grangers-CAR-Mac.local>
2022-11-17 15:38:32 -0600

    Added Endscreen logic

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { QuizContext } from '../helpers/Contexts';
+import { Questions } from '../helpers/QuestionBank';
+import '../App.css';
 
 function EndScreen() {
+    const { score, setScore, setGameState } = useContext(QuizContext);
+
+    const restartQuiz = () => {
+        setScore(0);
+        setGameState("menu");
+    };
+
     return (
-        <div>EndScreen</div>
+        <div className='EndScreen'>
+            <h1>Quiz Finished</h1>
+            <h3> {score} / {Questions.length}</h3>
+            <button onClick={restartQuiz}> Restart Quiz </button>
+        </div>
     )
 }
 
